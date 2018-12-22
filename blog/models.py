64399f6031baba418dc229ced10bf7cac7a681fa
Jimmy9507 <406403730@qq.com>
2018-12-21 19:41:48 -0500

    first commit

---
+++ blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    #标题
+    title=models.CharField(max_length=32,default='Title')
+    #内容
+    content=models.TextField(null=True)
+    pub_time=models.DateTimeField(null=True)
+    def __unicode__(self):
+        return self.title
---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path,include,re_path
+
+from .import views
+app_name='blog'
+urlpatterns = [
+
+    path('index/',views.index),
+    re_path(r'^article/(?P<article_id>[0-9]+)$', views.article_page, name='article_page'),
+    re_path(r'^edit/(?P<article_id>[0-9]+)$', views.edit_page,name='edit_page'),
+    re_path(r'^edit/action/$', views.edit_action,name="edit_action"),
+    ]
